2d1ca571418db49e6ed9704c0df399b48ceeddca
lukeg <groeluke@isu.edu>
2026-03-10 08:25:54 -0600

    Add project files.

---
+++ ListExamples/ListExamplesForm.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace ListExamples
+{
+    partial class ListExamplesForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            ExitButton = new Button();
+            SuspendLayout();
+            // 
+            // ExitButton
+            // 
+            ExitButton.Location = new Point(634, 369);
+            ExitButton.Name = "ExitButton";
+            ExitButton.Size = new Size(154, 69);
+            ExitButton.TabIndex = 0;
+            ExitButton.Text = "E&xit";
+            ExitButton.UseVisualStyleBackColor = true;
+            ExitButton.Click += ExitButton_Click;
+            // 
+            // ListExamplesForm
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(ExitButton);
+            Name = "ListExamplesForm";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button ExitButton;
+    }
+}
---
+++ ListExamples/ListExamplesForm.cs
@@ -0,0 +1,18 @@
+
+
+namespace ListExamples
+{
+    public partial class ListExamplesForm : Form
+    {
+        public ListExamplesForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+    }
+}
